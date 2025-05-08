e6dbd77141a9d6d20dc1fa14ca656e4ef3e98c93
Zaoui-Douaa <zaouidoa9@gmail.com>
2025-05-08 19:23:11 +0000

    first commit

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
